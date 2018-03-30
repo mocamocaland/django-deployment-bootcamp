a751f93f7e68709779e9a7d36a1ae92c79097cba
mocamoca <noi_kaas@yahoo.co.jp>
2018-03-30 19:06:51 +0900

    add formUI of function

--- ProTwo/appTwo/views.py
+++ ProTwo/appTwo/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# from appTwo.models import User
+from appTwo.forms import NewUserForm
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<em>My Second Project</em>")
+    return render(request, 'appTwo/index.html')
+
+def users(request):
+    
+    form = NewUserForm()
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('ERROR FORM INVALID')
+
+    return render(request, 'apptwo/users.html', {'form': form})
+
