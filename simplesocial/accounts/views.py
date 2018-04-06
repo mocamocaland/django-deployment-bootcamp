98baba9d0445c7affeeef0f104b44aa78d208544
mocamoca <noi_kaas@yahoo.co.jp>
2018-04-06 19:16:33 +0900

    add formUI

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
 
+from django.vieews.generic import CreateView
+
+from . import forms
 # Create your views here.
+
+class SignUp(CreateView):
+    pass
