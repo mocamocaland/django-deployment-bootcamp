d2a34413a0ba77470207e07ea3358cf7f956d137
mocamocaland <noi_kaas@yahoo.co.jp>
2018-04-23 21:30:13 +0900

    temoplatesの配置を変更

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    my_dict = {'insert_me': "Hello I am from views.py !"}
-    return render(request, 'index.html', context=my_dict)
+    my_dict = {'insert_me': "Now I am coming from first_app/index.html!"}
+    return render(request, 'first_app/index.html', context=my_dict)
